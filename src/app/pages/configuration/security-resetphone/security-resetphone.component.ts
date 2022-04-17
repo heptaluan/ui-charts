da401f77e32a29db300b6f37ee9c9716ac079072
heptaluan <vd2o@foxmail.com>
2022-04-17 23:43:31 +0800

    add

--- src/app/pages/configuration/security-resetphone/security-resetphone.component.ts
+++ src/app/pages/configuration/security-resetphone/security-resetphone.component.ts
@@ -1,219 +1,215 @@
 /*
  * @Description: 个人设置-安全隐私-修改手机号
  */
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { ModalDirective } from 'ngx-bootstrap/modal';
-import { HttpService } from '../../../states/services/custom-http.service';
-import { API } from '../../../states/api.service';
-import { map, tap, switchMap, catchError } from 'rxjs/operators';
-import { empty } from 'rxjs/observable/empty';
-import { Observable } from 'rxjs';
-import * as ErrType from '../../../share/error.type';
-import { Store } from '@ngrx/store';
-import * as fromRoot from '../../../states/reducers';
-import * as UserActions from '../../../states/actions/user.action';
-import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, ViewChild, Input } from '@angular/core'
+import { ModalDirective } from 'ngx-bootstrap/modal'
+import { HttpService } from '../../../states/services/custom-http.service'
+import { API } from '../../../states/api.service'
+import { map, tap, switchMap, catchError } from 'rxjs/operators'
+import { empty } from 'rxjs/observable/empty'
+import { Observable } from 'rxjs'
+import * as ErrType from '../../../share/error.type'
+import { Store } from '@ngrx/store'
+import * as fromRoot from '../../../states/reducers'
+import * as UserActions from '../../../states/actions/user.action'
+import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http'
 
 @Component({
   selector: 'lx-security-resetphone',
   templateUrl: './security-resetphone.component.html',
-  styleUrls: ['./security-resetphone.component.scss']
+  styleUrls: ['./security-resetphone.component.scss'],
 })
 export class SecurityResetphoneComponent implements OnInit {
-  codestate = '';
-  errorTip = '';
-  sendCodeTip = '发送验证码';
-  title = '提示';
+  codestate = ''
+  errorTip = ''
+  sendCodeTip = '发送验证码'
+  title = '提示'
 
-  phoneCode = '';
-  phoneNo = '';
-  newPhoneCode = '';
-  newPhoneNo = '';
+  phoneCode = ''
+  phoneNo = ''
+  newPhoneCode = ''
+  newPhoneNo = ''
 
-  codeTimer = null;
+  codeTimer = null
 
   @ViewChild('resetPhoneModal', undefined)
-  resetPhoneModal: ModalDirective; // 修改密码
-  @ViewChild('rpAskDiv') rpAskDiv;
-  @ViewChild('rpSmsUselessDiv') rpSmsUselessDiv;
-  @ViewChild('rpSmsAvailableDiv') rpSmsAvailableDiv;
-  @ViewChild('rpVerityNewDiv') rpVerityNewDiv;
+  resetPhoneModal: ModalDirective // 修改密码
+  @ViewChild('rpAskDiv') rpAskDiv
+  @ViewChild('rpSmsUselessDiv') rpSmsUselessDiv
+  @ViewChild('rpSmsAvailableDiv') rpSmsAvailableDiv
+  @ViewChild('rpVerityNewDiv') rpVerityNewDiv
 
-  @Input() userphone: string;
+  @Input() userphone: string
 
-  constructor(private _http: HttpClient, private _api: API, private _store: Store<fromRoot.State>) {
-  }
+  constructor(private _http: HttpClient, private _api: API, private _store: Store<fromRoot.State>) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   show() {
-    this.resetPhoneModal.show();
+    this.resetPhoneModal.show()
   }
 
   hide() {
-    this.resetPhoneModal.hide();
+    this.resetPhoneModal.hide()
     // 将弹框状态还原
     setTimeout(() => {
-      this.title = '提示';
-      this.rpAskDiv.nativeElement.hidden = false;
-      this.rpSmsUselessDiv.nativeElement.hidden = true;
-      this.rpSmsAvailableDiv.nativeElement.hidden = true;
-      this.rpVerityNewDiv.nativeElement.hidden = true;
-      this.resetInput();
-    }, 500);
+      this.title = '提示'
+      this.rpAskDiv.nativeElement.hidden = false
+      this.rpSmsUselessDiv.nativeElement.hidden = true
+      this.rpSmsAvailableDiv.nativeElement.hidden = true
+      this.rpVerityNewDiv.nativeElement.hidden = true
+      this.resetInput()
+    }, 500)
   }
 
   resetInput() {
-    this.errorTip = '';
-    this.phoneCode = '';
-    this.phoneNo = '';
-    this.newPhoneCode = '';
-    this.newPhoneNo = '';
+    this.errorTip = ''
+    this.phoneCode = ''
+    this.phoneNo = ''
+    this.newPhoneCode = ''
+    this.newPhoneNo = ''
   }
 
   onKeyDown(event) {
-    this.errorTip = '';
+    this.errorTip = ''
   }
 
   codeCountDown() {
-    let count = 60;
+    let count = 60
     if (this.codeTimer) {
-      clearInterval(this.codeTimer);
-      this.codeTimer = null;
+      clearInterval(this.codeTimer)
+      this.codeTimer = null
     }
     this.codeTimer = setInterval(() => {
       if (count >= 1) {
-        this.sendCodeTip = `${count}秒后重新发送`;
-        this.codestate = `disabled btn-disable`;
+        this.sendCodeTip = `${count}秒后重新发送`
+        this.codestate = `disabled btn-disable`
       } else {
-        this.sendCodeTip = `发送验证码`;
-        this.codestate = ``;
-        clearInterval(this.codeTimer);
-        this.codeTimer = null;
+        this.sendCodeTip = `发送验证码`
+        this.codestate = ``
+        clearInterval(this.codeTimer)
+        this.codeTimer = null
       }
-      count--;
-    }, 1000);
+      count--
+    }, 1000)
   }
 
   sendSMS(phoneNo: string, step: string) {
     if (!phoneNo.trim()) {
-      this.errorTip = '请输入手机号码';
-      return;
+      this.errorTip = '请输入手机号码'
+      return
     }
     if (phoneNo.trim().length !== 11) {
-      this.errorTip = '请输入正确的手机号';
-      return;
+      this.errorTip = '请输入正确的手机号'
+      return
     }
-    this.sendCodeTip = `验证码发送中...`;
-    this.codestate = `disabled`;
-    const url = this._api.resetPhoneSendSmsCode();
-    const form = new FormData();
-    form.append('phoneNo', phoneNo);
-    form.append('step', step);
-    this._http.post(url, form, { withCredentials: true })
-      .subscribe(
-        (res: any) => {
-          const errTip = ErrType.getError(res.resultCode);
-          if (errTip) {
-            this.errorTip = errTip;
-            this.sendCodeTip = `发送验证码`;
-            this.codestate = ``;
-          } else {
-            this.codeCountDown();
-          }
-        },
-        error => {
-          alert(error.message);
+    this.sendCodeTip = `验证码发送中...`
+    this.codestate = `disabled`
+    const url = this._api.resetPhoneSendSmsCode()
+    const form = new FormData()
+    form.append('phoneNo', phoneNo)
+    form.append('step', step)
+    this._http.post(url, form, { withCredentials: true }).subscribe(
+      (res: any) => {
+        const errTip = ErrType.getError(res.resultCode)
+        if (errTip) {
+          this.errorTip = errTip
+          this.sendCodeTip = `发送验证码`
+          this.codestate = ``
+        } else {
+          this.codeCountDown()
         }
-      );
+      },
+      (error) => {
+        alert(error.message)
+      }
+    )
   }
 
   verifySMS(phoneNo: string, phoneCode: string, step: string) {
     if (!phoneNo.trim()) {
-      this.errorTip = '请输入手机号码';
-      return;
+      this.errorTip = '请输入手机号码'
+      return
     }
     if (phoneNo.trim().length !== 11) {
-      this.errorTip = '请输入正确的手机号';
-      return;
+      this.errorTip = '请输入正确的手机号'
+      return
     }
     if (!phoneCode.trim()) {
-      this.errorTip = '请输入验证码';
-      return;
+      this.errorTip = '请输入验证码'
+      return
     }
-    const url = this._api.resetPhoneVerify();
-    const form = new FormData();
-    form.append('phoneNo', phoneNo);
-    form.append('validateCode', phoneCode);
-    form.append('step', step);
-    this._http.post(url, form, { withCredentials: true })
-      .subscribe(
-        (res: any) => {
-          const errTip = ErrType.getError(res.resultCode);
-          if (errTip) {
-            this.errorTip = errTip;
-          } else {
-            if (this.codeTimer) {
-              this.sendCodeTip = `发送验证码`;
-              this.codestate = ``;
-              clearInterval(this.codeTimer);
-              this.codeTimer = null;
-            }
-            if (step === '1') {
-              this.title = '请验证新手机';
-              this.rpSmsAvailableDiv.nativeElement.hidden = true;
-              this.rpVerityNewDiv.nativeElement.hidden = false;
-            } else {
-              this._store.dispatch(new UserActions.GetUserInfoAction());
-              this.hide();
-              alert('手机修改成功！');
-            }
+    const url = this._api.resetPhoneVerify()
+    const form = new FormData()
+    form.append('phoneNo', phoneNo)
+    form.append('validateCode', phoneCode)
+    form.append('step', step)
+    this._http.post(url, form, { withCredentials: true }).subscribe(
+      (res: any) => {
+        const errTip = ErrType.getError(res.resultCode)
+        if (errTip) {
+          this.errorTip = errTip
+        } else {
+          if (this.codeTimer) {
+            this.sendCodeTip = `发送验证码`
+            this.codestate = ``
+            clearInterval(this.codeTimer)
+            this.codeTimer = null
           }
-        },
-        error => {
-          alert(error.message);
+          if (step === '1') {
+            this.title = '请验证新手机'
+            this.rpSmsAvailableDiv.nativeElement.hidden = true
+            this.rpVerityNewDiv.nativeElement.hidden = false
+          } else {
+            this._store.dispatch(new UserActions.GetUserInfoAction())
+            this.hide()
+            alert('手机修改成功！')
+          }
         }
-      );
+      },
+      (error) => {
+        alert(error.message)
+      }
+    )
   }
 
   // 用户操作方法
   // 点击旧手机可用
   onPhoneUsefull() {
-    this.title = '请验证已绑定的手机';
-    this.rpAskDiv.nativeElement.hidden = true;
-    this.rpSmsAvailableDiv.nativeElement.hidden = false;
+    this.title = '请验证已绑定的手机'
+    this.rpAskDiv.nativeElement.hidden = true
+    this.rpSmsAvailableDiv.nativeElement.hidden = false
   }
 
   // 点击旧手机不可用
   onPhoneUseless() {
-    this.title = '联系镝数小助手';
-    this.rpAskDiv.nativeElement.hidden = true;
-    this.rpSmsUselessDiv.nativeElement.hidden = false;
+    this.title = '联系镝数小助手'
+    this.rpAskDiv.nativeElement.hidden = true
+    this.rpSmsUselessDiv.nativeElement.hidden = false
   }
 
   // 联系客服页面完成
   onServiceDone() {
-    this.hide();
+    this.hide()
   }
 
   // 旧手机发送验证码
   onOldSendSms() {
-    this.sendSMS(this.phoneNo, '1');
+    this.sendSMS(this.phoneNo, '1')
   }
 
   // 旧手机验证码下一步
   onOldPhoneNext() {
-    this.verifySMS(this.phoneNo, this.phoneCode, '1');
+    this.verifySMS(this.phoneNo, this.phoneCode, '1')
   }
 
   // 新手机发送验证码
   onNewSendSms() {
-    this.sendSMS(this.newPhoneNo, '2');
+    this.sendSMS(this.newPhoneNo, '2')
   }
 
   // 新手机完成
   onNewPhoneDone() {
-    this.verifySMS(this.newPhoneNo, this.newPhoneCode, '2');
+    this.verifySMS(this.newPhoneNo, this.newPhoneCode, '2')
   }
 }
--- src/app/pages/configuration/security/security.component.ts
+++ src/app/pages/configuration/security/security.component.ts
@@ -1,52 +1,73 @@
 /*
  * @Description: 个人设置-安全隐私
  */
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { SecurityResetpwComponent } from '../security-resetpw/security-resetpw.component';
-import { SecurityResetphoneComponent } from '../security-resetphone/security-resetphone.component';
-import { SecurityResetmailComponent } from '../security-resetmail/security-resetmail.component';
-import { Store } from '@ngrx/store';
-import * as fromRoot from '../../../states/reducers';
-import * as UserModels from '../../../states/models/user.model';
-import { Subscription, Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core'
+import { SecurityResetpwComponent } from '../security-resetpw/security-resetpw.component'
+import { SecurityResetphoneComponent } from '../security-resetphone/security-resetphone.component'
+import { SecurityResetmailComponent } from '../security-resetmail/security-resetmail.component'
+import { Store } from '@ngrx/store'
+import * as fromRoot from '../../../states/reducers'
+import * as UserModels from '../../../states/models/user.model'
+import { Subscription, Observable } from 'rxjs'
+import { ModalDirective } from 'ngx-bootstrap/modal'
+import { HttpService } from '../../../states/services/custom-http.service'
+import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http'
+import { API } from '../../../states/api.service'
+import { map, tap, switchMap, catchError } from 'rxjs/operators'
+import { empty } from 'rxjs/observable/empty'
+import * as ErrType from '../../../share/error.type'
 
 @Component({
   selector: 'lx-security',
   templateUrl: './security.component.html',
-  styleUrls: ['./security.component.scss']
+  styleUrls: ['./security.component.scss'],
 })
-
 export class SecurityComponent implements OnInit, OnDestroy {
   @ViewChild('resetPwChild', undefined)
-  resetPwChild: SecurityResetpwComponent;       // 修改密码
+  resetPwChild: SecurityResetpwComponent // 修改密码
   @ViewChild('resetPhoneChild', undefined)
-  resetPhoneChild: SecurityResetphoneComponent; // 修改手机号
+  resetPhoneChild: SecurityResetphoneComponent // 修改手机号
   @ViewChild('resetMailChild', undefined)
-  resetMailChild: SecurityResetmailComponent;   // 绑定邮箱
+  resetMailChild: SecurityResetmailComponent // 绑定邮箱
 
-  userinfo$: Observable<UserModels.UserInfo>;
-  userinfoSubscribe = new Subscription();
-  userinfo: any;
-  email = '';
-  securityEmail = '';
-  phoneno = '';
+  userinfo$: Observable<UserModels.UserInfo>
+  userinfoSubscribe = new Subscription()
+  userinfo: any
+  email = ''
+  securityEmail = ''
+  phoneno = ''
 
-  constructor(private _store: Store<fromRoot.State>) {
-    this.userinfo$ = this._store.select(fromRoot.getUserInfo);
+  errorTip = ''
+  title = '请输入登录密码'
+
+  password = ''
+  type = '1' // 1是没有绑定邮箱， 2是已经绑定邮箱
+  oldEmail = ''
+  newEmail = ''
+
+  @ViewChild('resetMailModal', undefined)
+  resetMailModal: ModalDirective // 修改密码
+  @ViewChild('rmVerifyPwdDiv') rmVerifyPwdDiv
+  @ViewChild('rmBindNewMailDiv') rmBindNewMailDiv
+  @ViewChild('rmResetMailDiv') rmResetMailDiv
+  @ViewChild('rmBindSuccessDiv') rmBindSuccessDiv
+
+  constructor(private _store: Store<fromRoot.State>, private _http: HttpClient, private _api: API) {
+    this.userinfo$ = this._store.select(fromRoot.getUserInfo)
   }
 
   ngOnInit() {
     // 百度统计
     // window['_hmt'].push(['_trackPageview', '/appv2/#/pages/configuration/security']);
 
-    this.userinfoSubscribe = this.userinfo$.subscribe(info => {
+    this.userinfoSubscribe = this.userinfo$.subscribe((info) => {
       if (info) {
-        this.userinfo = info;
-        this.email = info.email ? info.email.trim() : '';
-        this.securityEmail = this.email.slice(0, 2) + '***' + this.email.slice(this.email.length - 3);
-        this.phoneno = info.phoneno ? info.phoneno.trim() : '';
+        this.userinfo = info
+        this.email = info.email ? info.email.trim() : ''
+        this.securityEmail = this.email.slice(0, 2) + '***' + this.email.slice(this.email.length - 3)
+        this.phoneno = info.phoneno ? info.phoneno.trim() : ''
       }
-    });
+    })
   }
 
   openModal(index) {
@@ -54,39 +75,163 @@ export class SecurityComponent implements OnInit, OnDestroy {
       // 修改密码
       case 0:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'me', 'me', 'me-change-password']);
-        this.resetPw();
-        break;
+        window['_hmt'].push(['_trackEvent', 'me', 'me', 'me-change-password'])
+        this.resetPw()
+        break
       // 修改手机号
       case 1:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'me', 'me', 'me-change-phone']);
-        this.resetPhone();
-        break;
+        window['_hmt'].push(['_trackEvent', 'me', 'me', 'me-change-phone'])
+        this.resetPhone()
+        break
       // 修改邮箱
       case 2:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'me', 'me', 'me-change-email']);
-        this.resetMail();
-        break;
+        window['_hmt'].push(['_trackEvent', 'me', 'me', 'me-change-email'])
+        this.resetMail()
+        break
       default:
-        break;
+        break
     }
   }
 
   resetPw() {
-    this.resetPwChild.show(this.email);
+    this.resetPwChild.show(this.email)
   }
 
   resetPhone() {
-    this.resetPhoneChild.show();
+    this.resetPhoneChild.show()
   }
 
   resetMail() {
-    this.resetMailChild.show(this.email);
+    this.resetMailChild.show(this.email)
   }
 
   ngOnDestroy() {
-    this.userinfoSubscribe.unsubscribe();
+    this.userinfoSubscribe.unsubscribe()
+  }
+
+  show(email) {
+    this.type = !email.trim() ? '1' : '2'
+    this.resetMailModal.show()
+  }
+
+  hide() {
+    this.resetMailModal.hide()
+    setTimeout(() => {
+      this.title = '请输入登录密码'
+      this.rmVerifyPwdDiv.nativeElement.hidden = false
+      this.rmBindNewMailDiv.nativeElement.hidden = true
+      this.rmResetMailDiv.nativeElement.hidden = true
+      this.rmBindSuccessDiv.nativeElement.hidden = true
+      this.resetInput()
+    }, 500)
+  }
+
+  resetInput() {
+    this.errorTip = ''
+    this.password = ''
+    this.oldEmail = ''
+    this.newEmail = ''
+  }
+
+  onKeyDown(event) {
+    this.errorTip = ''
+  }
+
+  // 验证登录密码
+  onVerityPwd() {
+    if (!this.password.trim()) {
+      this.errorTip = '请输入登录密码'
+      return
+    }
+    const url = this._api.resetEmailVerifyPwd()
+    const form = new FormData()
+    form.append('password', this.password)
+    form.append('type', this.type)
+    this._http.post(url, form, { withCredentials: true }).subscribe(
+      (res: any) => {
+        const errTip = ErrType.getError(res.resultCode)
+        if (errTip) {
+          this.errorTip = errTip
+        } else {
+          if (this.type === '2') {
+            // 已绑定邮箱
+            this.title = '请输入已绑定的邮箱'
+            this.rmVerifyPwdDiv.nativeElement.hidden = true
+            this.rmResetMailDiv.nativeElement.hidden = false
+          } else {
+            this.title = '绑定邮箱信息'
+            this.rmVerifyPwdDiv.nativeElement.hidden = true
+            this.rmBindNewMailDiv.nativeElement.hidden = false
+          }
+        }
+      },
+      (error: any) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  // 绑定新邮箱完成
+  onBindNewMail() {
+    if (!this.newEmail.trim()) {
+      this.errorTip = '邮箱不能为空'
+      return
+    }
+    const url = this._api.resetEmailSetNew()
+    const form = new FormData()
+    form.append('email', this.newEmail)
+    form.append('type', this.type)
+    this._http.post(url, form, { withCredentials: true }).subscribe(
+      (res: any) => {
+        const errTip = ErrType.getError(res.resultCode)
+        if (errTip) {
+          this.errorTip = errTip
+        } else {
+          this.title = '邮箱链接发送成功'
+          this.rmBindNewMailDiv.nativeElement.hidden = true
+          this.rmBindSuccessDiv.nativeElement.hidden = false
+        }
+      },
+      (error: any) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  // 解绑旧邮箱下一步
+  onResetOldMail() {
+    if (!this.oldEmail.trim()) {
+      this.errorTip = '请输入已绑定的邮箱'
+      return
+    }
+    let reg = new RegExp('^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$')
+    if (!reg.test(this.oldEmail.trim())) {
+      this.errorTip = '请输入正确的邮箱地址'
+      return
+    }
+    const url = this._api.resetEmailVerifyOld()
+    const form = new FormData()
+    form.append('email', this.oldEmail)
+    this._http.post(url, form, { withCredentials: true }).subscribe(
+      (res: any) => {
+        const errTip = ErrType.getError(res.resultCode)
+        if (errTip) {
+          this.errorTip = errTip
+        } else {
+          this.title = '绑定邮箱信息'
+          this.rmResetMailDiv.nativeElement.hidden = true
+          this.rmBindNewMailDiv.nativeElement.hidden = false
+        }
+      },
+      (error: any) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  onBindSuccess() {
+    this.hide()
   }
 }
