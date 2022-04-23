283d469085b4b84d0e6d501374a80bc41a40ad8e
heptaluan <vd2o@foxmail.com>
2022-04-23 21:01:03 +0800

    add

--- src/app/pages/configuration/configuration.component.ts
+++ src/app/pages/configuration/configuration.component.ts
@@ -1,116 +1,121 @@
 /*
  * @Description: 个人设置页面
  */
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { Store } from '@ngrx/store';
-import * as fromRoot from '../../states/reducers';
-import * as UserActions from '../../states/actions/user.action';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core'
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router'
+import { Store } from '@ngrx/store'
+import * as fromRoot from '../../states/reducers'
+import * as UserActions from '../../states/actions/user.action'
 
-import { Observable } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
-import { UtilsService } from '../../share/services/utils.service';
+import { Observable } from 'rxjs'
+import { debounceTime, map } from 'rxjs/operators'
+import { UtilsService } from '../../share/services/utils.service'
 
 @Component({
   selector: 'lx-configuration',
   templateUrl: './configuration.component.html',
-  styleUrls: ['./configuration.component.scss']
+  styleUrls: ['./configuration.component.scss'],
 })
 export class ConfigurationComponent implements OnInit, AfterViewInit {
-  @ViewChild('menuItems') menuItems;
-  changeWidth;
-  changeHeight;
+  @ViewChild('menuItems') menuItems
+  changeWidth
+  changeHeight
 
-  browserType: string = '';
-  isChrome: boolean = true;
-  isCloseTip: boolean = false;
+  browserType: string = ''
+  isChrome: boolean = true
+  isCloseTip: boolean = false
 
   menuList = [
-    { 'title': '个人设置', 'path': 'settings' },
-    { 'title': '安全设置', 'path': 'security' },
-    { 'title': '我的会员', 'path': 'plan' },
-    { 'title': '我的账单', 'path': 'bill' },
-    { 'title': '特权兑换', 'path': 'exchange' },
-    { 'title': '邀请有礼', 'path': 'invite' },
-  ];
+    { title: '个人设置', path: 'settings' },
+    { title: '安全设置', path: 'security' },
+    { title: '我的会员', path: 'plan' },
+    { title: '我的账单', path: 'bill' },
+    { title: '特权兑换', path: 'exchange' },
+    { title: '邀请有礼', path: 'invite' },
+  ]
 
   constructor(
     private router: Router,
     private activedRouter: ActivatedRoute,
     private _store: Store<fromRoot.State>,
     private _utilService: UtilsService
-    ) {
-    router.events.filter(event => event instanceof NavigationEnd).subscribe((info: NavigationEnd) => {
-      this.activeMenu(info.url);
-    });
+  ) {
+    router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .subscribe((info: NavigationEnd) => {
+        this.activeMenu(info.url)
+      })
   }
 
   ngOnInit() {
     // this._store.dispatch(new UserActions.GetUserBillAction());
 
     // 判断谷歌浏览器
-    this.isChrome = this._utilService.getBrowserInfo() === 'Chrome' || this._utilService.getBrowserInfo() === 'Safari';
-    this.isCloseTip = this._utilService.isCloseTip;
-
+    this.isChrome = this._utilService.getBrowserInfo() === 'Chrome' || this._utilService.getBrowserInfo() === 'Safari'
+    this.isCloseTip = this._utilService.isCloseTip
 
     // 初始化导航栏宽度
-    this.resetWidth();
+    this.resetWidth()
 
     // 改变窗口大小实时改变导航栏宽度
     Observable.fromEvent(window, 'resize')
       .pipe(
-        debounceTime(100), // 加点去抖，毕竟 `resize` 频率非常高
+        debounceTime(100) // 加点去抖，毕竟 `resize` 频率非常高
       )
       .subscribe((event) => {
-        this.resetWidth();
-      });
+        this.resetWidth()
+      })
   }
 
   ngAfterViewInit() {
-    const url = this.router.url;
-    this.activeMenu(url);
+    const url = this.router.url
+    this.activeMenu(url)
   }
 
   // 重新设置导航栏宽度
   resetWidth() {
-    let nowWidth = document.documentElement.clientWidth;
-    let nowHeight = document.documentElement.clientHeight;
+    let nowWidth = document.documentElement.clientWidth
+    let nowHeight = document.documentElement.clientHeight
     if (nowWidth > 1263) {
-      this.changeWidth = Math.floor(nowWidth - 63) + 'px';
+      this.changeWidth = Math.floor(nowWidth - 63) + 'px'
     } else {
-      this.changeWidth = '1200px';
+      this.changeWidth = '1200px'
     }
-    this.changeHeight = nowHeight - 60 + 'px';
+    this.changeHeight = nowHeight - 60 + 'px'
   }
 
   // 关闭提示语
   closeTip() {
-    this.isCloseTip = true;
-    this._utilService.closeTip();
+    this.isCloseTip = true
+    this._utilService.closeTip()
   }
 
   activeMenu(url) {
-    const pathArr = url.split('/');
-    const path = pathArr[pathArr.length - 1];
-    if (!this.menuItems || !this.menuItems.nativeElement.children || this.menuItems.nativeElement.children.length === 0) {
-      return;
+    const pathArr = url.split('/')
+    const path = pathArr[pathArr.length - 1]
+    if (
+      !this.menuItems ||
+      !this.menuItems.nativeElement.children ||
+      this.menuItems.nativeElement.children.length === 0
+    ) {
+      return
     }
     for (const item of this.menuItems.nativeElement.children) {
-      item.classList.remove('active');
+      item.classList.remove('active')
     }
-    let i = 0;
+    let i = 0
     for (const item of this.menuList) {
       if (item.path === path) {
-        this.menuItems.nativeElement.children[i].classList.add('active');
-        break;
+        this.menuItems.nativeElement.children[i].classList.add('active')
+        break
       }
-      i++;
+      i++
     }
   }
 
   listenFromChild(msg: string) {
     if (msg === '[Router] Go To Member') {
-      this.onMenuClicked(undefined, 3);
+      this.onMenuClicked(undefined, 3)
     }
   }
 
@@ -118,31 +123,31 @@ export class ConfigurationComponent implements OnInit, AfterViewInit {
     switch (index) {
       case 0:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-settings']);
-        break;
+        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-settings'])
+        break
       case 1:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-security']);
-        break;
+        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-security'])
+        break
       case 2:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-membership']);
-        break;
+        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-membership'])
+        break
       case 3:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-bill']);
-        break;
+        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-bill'])
+        break
       case 4:
         // 百度统计
-        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-invite']);
-        break;
+        window['_hmt'].push(['_trackEvent', 'navigation', 'navi-me', 'navi-my-invite'])
+        break
       default:
-        break;
+        break
     }
     for (const item of this.menuItems.nativeElement.children) {
-      item.classList.remove('active');
+      item.classList.remove('active')
     }
-    this.menuItems.nativeElement.children[index].classList.add('active');
-    this.router.navigate(['pages', 'configuration', this.menuList[index].path]);
+    this.menuItems.nativeElement.children[index].classList.add('active')
+    this.router.navigate(['pages', 'configuration', this.menuList[index].path])
   }
 }
--- src/app/pages/configuration/configuration.module.ts
+++ src/app/pages/configuration/configuration.module.ts
@@ -1,40 +1,30 @@
 /*
  * @Description: 个人设置页模块
  */
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
-import { ShareModule } from '../../share/share.module';
-import { ImageCropperModule } from 'ngx-img-cropper';
+import { ShareModule } from '../../share/share.module'
+import { ImageCropperModule } from 'ngx-img-cropper'
 
-import { ConfigurationRoutingModule } from './configuration-routing.module';
-import { ConfigurationComponent} from './configuration.component';
-import { SettingsComponent } from './settings/settings.component';
-import { SecurityComponent } from './security/security.component';
-import { BillComponent } from './bill/bill.component';
-import { PlanComponent } from './plan/plan.component';
-import { MemberComponent } from './member/member.component';
-import { BeginnerComponent } from './beginner/beginner.component';
-import { SecurityResetpwComponent } from './security-resetpw/security-resetpw.component';
-import { SecurityResetphoneComponent } from './security-resetphone/security-resetphone.component';
-import { SecurityResetmailComponent } from './security-resetmail/security-resetmail.component';
-import { InviteComponent } from './invite/invite.component';
-import { ExchangeComponent } from './exchange/exchange.component';
+import { ConfigurationRoutingModule } from './configuration-routing.module'
+import { ConfigurationComponent } from './configuration.component'
+import { SettingsComponent } from './settings/settings.component'
+import { SecurityComponent } from './security/security.component'
+import { BillComponent } from './bill/bill.component'
+import { PlanComponent } from './plan/plan.component'
+import { MemberComponent } from './member/member.component'
+import { BeginnerComponent } from './beginner/beginner.component'
+import { SecurityResetpwComponent } from './security-resetpw/security-resetpw.component'
+import { SecurityResetphoneComponent } from './security-resetphone/security-resetphone.component'
+import { SecurityResetmailComponent } from './security-resetmail/security-resetmail.component'
+import { InviteComponent } from './invite/invite.component'
+import { ExchangeComponent } from './exchange/exchange.component'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ShareModule,
-    ConfigurationRoutingModule,
-    FormsModule,
-    ImageCropperModule
-  ],
-  exports: [
-    PlanComponent,
-    BillComponent,
-    ExchangeComponent
-  ],
+  imports: [CommonModule, ShareModule, ConfigurationRoutingModule, FormsModule, ImageCropperModule],
+  exports: [PlanComponent, BillComponent, ExchangeComponent],
   declarations: [
     ConfigurationComponent,
     SettingsComponent,
@@ -48,6 +38,6 @@ import { ExchangeComponent } from './exchange/exchange.component';
     SecurityResetphoneComponent,
     SecurityResetmailComponent,
     ExchangeComponent,
-  ]
+  ],
 })
-export class ConfigurationModule { }
+export class ConfigurationModule {}
