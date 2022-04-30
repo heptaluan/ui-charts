68235fdcfdbf9ba343e9941aa7e7fa921a30609b
heptaluan <vd2o@foxmail.com>
2022-04-30 23:34:26 +0800

    add

--- src/app/pages/home-page/cases/cases.component.ts
+++ src/app/pages/home-page/cases/cases.component.ts
@@ -1,36 +1,37 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Store } from '@ngrx/store';
-import * as CaseActions from '../../../states/actions/project-case.action';
-import * as fromRoot from '../../../states/reducers';
-import { Subscription } from 'rxjs';
-import { take } from 'rxjs/operators';
-import * as ProjectActions from '../../../states/actions/project.action';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { Store } from '@ngrx/store'
+import * as CaseActions from '../../../states/actions/project-case.action'
+import * as fromRoot from '../../../states/reducers'
+import { Subscription } from 'rxjs'
+import { take } from 'rxjs/operators'
+import * as ProjectActions from '../../../states/actions/project.action'
 
 @Component({
   selector: 'lx-cases',
   templateUrl: './cases.component.html',
-  styleUrls: ['./cases.component.scss']
+  styleUrls: ['./cases.component.scss'],
 })
-
 export class CasesComponent implements OnInit, OnDestroy {
-
-  initSearch: string = '';
-  mySubscription = new Subscription();
+  initSearch: string = ''
+  mySubscription = new Subscription()
 
   constructor(private _store: Store<fromRoot.State>) {
-    this._store.dispatch(new ProjectActions.GetProjectListAction());
-    this.mySubscription.add(this._store.select(fromRoot.getProjectList).subscribe())
-      .add(this._store.select(fromRoot.getProjectCaseSearch).pipe(take(1)).subscribe(search => this.initSearch = search));
+    this._store.dispatch(new ProjectActions.GetProjectListAction())
+    this.mySubscription.add(this._store.select(fromRoot.getProjectList).subscribe()).add(
+      this._store
+        .select(fromRoot.getProjectCaseSearch)
+        .pipe(take(1))
+        .subscribe((search) => (this.initSearch = search))
+    )
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   ngOnDestroy(): void {
-    this.mySubscription.unsubscribe();
+    this.mySubscription.unsubscribe()
   }
 
   search(evt) {
-    this._store.dispatch(new CaseActions.SetProjectCaseSearchAction(evt));
+    this._store.dispatch(new CaseActions.SetProjectCaseSearchAction(evt))
   }
-
 }
